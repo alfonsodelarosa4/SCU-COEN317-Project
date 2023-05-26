4dec1fdd4e1379c176d459969d3fee6540bb0659
alfonsodelarosa4 <45708422+alfonsodelarosa4@users.noreply.github.com>
2023-05-25 19:24:07 -0700

    added mongodb databases, CRUD operations, and readwritelocks

--- main_cluster/backend-app/app.py
+++ main_cluster/backend-app/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, jsonify, session, request, g
+from flask_apscheduler import APScheduler
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 import requests, hashlib, logging,time
 from collections import defaultdict
+import requests, socket, logging, time, sys, threading,os
 
 app = Flask(__name__)
+
+# scheduler
+scheduler = APScheduler()
 
 '''
 PRINT STATEMENTS DO NOT WORK
@@ -23,6 +30,203 @@ global_var = defaultdict(lambda: None)
 # CONSTANTS
 M_BITS = 64
 RETRY_COUNT = 3
+
+# SQL database
+mongo_client = MongoClient('mongodb://localhost:27017')
+db = mongo_client.my_database
+
+users_db = db['users']
+
+p2pnodes_db = db['p2p_nodes']
+topics_db = db['topics']
+leaders_db = db['leaders']
+topic_members_db = db['topic_members_db']
+
+# lock mechanism
+class ReadAndWriteLock():
+    def __init__(self):
+        self.write_lock = threading.Lock()
+        self.readers_lock = threading.Lock()
+        self.readers_count = 0
+    
+    def acquire_writelock(self):
+        self.write_lock.acquire()
+
+    def release_writelock(self):
+        self.write_lock.release()
+
+    def acquire_readlock(self):
+        self.readers_lock.acquire()
+        self.readers_count += 1
+        if self.readers_count == 1:
+            self.write_lock.acquire()
+        self.readers_lock.release()
+
+    def release_readlock(self):
+        self.readers_lock.acquire()
+        self.readers_count -= 1
+        if self.readers_count == 0:
+            self.write_lock.release()
+        self.readers_lock.release()
+    
+rw_locks = defaultdict(lambda:ReadAndWriteLock())
+
+class P2PNode:
+    def __init__(self,ip_address):
+        self.ip_address = ip_address
+
+class Topic:
+    def __init__(self,name):
+        self.name = name
+
+class Leader:
+    def __init__(self,ip_address):
+        self.ip_address = ip_address
+
+class TopicMember:
+    def __init__(self,topic,ip_address):
+        self.ip_address = ip_address
+        self.topic = topic
+
+# P2PNodes
+# create p2pnode
+def create_p2pnode(ip_address):
+    rw_locks["p2pnode"].acquire_writelock()
+    try:
+        new_p2pnode = P2PNode(ip_address)
+        p2p_id = p2pnodes_db.insert_one(new_p2pnode.__dict__).inserted_id
+        p2pnode = p2pnodes_db.find_one({'_id': ObjectId(p2p_id)})
+        p2pnode["_id"] = str(p2pnode["_id"])
+        return p2pnode
+    except:
+        app.logger.error("create_p2pnode")
+        return None
+    finally:
+        rw_locks["p2pnode"].release_writelock()
+
+# get p2pnode
+def get_p2pnode(ip_address):
+    rw_locks["p2pnode"].acquire_readlock()
+    if ip_address == None or ip_address == "":
+        rw_locks["p2pnode"].release_writelock()
+        app.logger.error('user_id is missing')
+        return
+    p2pnode = p2pnodes_db.find_one({'ip_address': ip_address})
+
+    if p2pnode:
+        p2pnode["_id"] = str(p2pnode["_id"])
+        rw_locks["p2pnode"].release_readlock()
+        return p2pnode
+    else:
+        rw_locks["p2pnode"].release_readlock()
+        app.logger.error('p2pnode not found')
+
+# delete p2pnode
+def delete_p2pnode(ip_address):
+    rw_locks["p2pnode"].acquire_writelock()
+    result = p2pnodes_db.delete_one({'ip_address': ip_address})
+    if result.deleted_count != 1:
+        app.logger.error('User not found')
+    rw_locks["p2pnode"].release_writelock()
+
+# TOPIC
+def create_topic(name):
+    rw_locks["topic"].acquire_writelock()
+    topic = topics_db.find_one({'name':name})
+    if topic is not None:
+        rw_locks["topic"].release_writelock()
+        app.logger.debug("topic already exists")
+        return False
+    new_topic = Topic(name=name)
+    topic_id = topics_db.insert_one(new_topic.__dict__).inserted_id
+    rw_locks["topic"].release_writelock()
+
+# get subscribed topics
+def get_topics():
+    rw_locks["topic"].acquire_readlock()
+    topics = topics_db.find()
+    rw_locks["topic"].release_readlock()   
+    return [topic["name"] for topic in topics]
+
+# get specific subscribed topic
+def get_topic(name):
+    rw_locks["topic"].acquire_readlock()
+    topic = topics_db.find_one({'name':name})
+    rw_locks["topic"].release_readlock()   
+    return topic
+
+def delete_topic(name):
+    rw_locks["topic"].acquire_writelock()
+    result = topics_db.delete_one({'name': name})
+    if result.deleted_count != 1:
+        app.logger.debug("topic not found")
+    rw_locks["topic"].release_writelock()
+
+
+# LEADER
+# set the leader
+def set_leader(ip_address):
+    rw_locks["leader"].acquire_writelock()
+    for leader in leaders_db.find():
+        leader__id = str(leader['_id'])
+        result = leaders_db.delete_one({'_id': ObjectId(leader__id)})
+        if result.deleted_count != 1:
+            app.logger.error('Leader not found')
+        
+    new_leader = Leader(ip_address=ip_address)
+    leaders_db.insert_one(new_leader.__dict__).inserted_id
+    rw_locks["leader"].release_writelock()
+
+# get the leader
+# return ip address of the leader
+def get_leader():
+    rw_locks["leader"].acquire_readlock()
+    leader = leaders_db.find_one()
+    rw_locks["leader"].release_readlock()
+
+    if leader is not None:
+        app.logger.debug("leader found")
+        return leader
+    else:
+        app.logger.debug("leader not found")
+        return None
+
+# TopicMember
+# create topic member
+def create_topic_member(ip_address,topic):
+    rw_locks["topic-member"].acquire_writelock()
+    new_topic = TopicMember(ip_address=ip_address,topic=topic)
+    topic_id = topic_members_db.insert_one(new_topic.__dict__).inserted_id
+    rw_locks["topic-member"].release_writelock()
+    return str(topic_id)
+
+# get topic members
+# return list of ip addresses
+def get_topic_members(topic):
+    rw_locks["topic-member"].acquire_readlock()
+    members = topic_members_db.find({'topic':topic})
+    rw_locks["topic-member"].release_readlock()
+    return [member["ip_address"] for member in members]
+
+# gets ip addresses of each topic member
+# return list of ip addresses
+def get_topic_members_from_all_topics():
+    rw_locks["topic-member"].acquire_readlock()
+    members = topic_members_db.find()
+    rw_locks["topic-member"].release_readlock()
+    return list(set([member["ip_address"] for member in members]))
+
+# delete a member from all topics
+def delete_member_from_all_topics(ip_address):
+    rw_locks["topic-member"].acquire_writelock()
+    result = topic_members_db.delete_many({'ip_address':ip_address})
+    rw_locks["topic-member"].release_writelock()
+
+# delete all members of from a topics
+def delete_all_members_from_a_topic(topic):
+    rw_locks["topic-member"].acquire_writelock()
+    results = topic_members_db.delete_many({'topic':topic})
+    rw_locks["topic-member"].release_writelock()
 
 '''
 given a request_func, attempt_request attempts to
@@ -53,14 +257,14 @@ def attempt_request(request_func):
     return None
 
 # get topics
+# NEEDS TO CHANGE
 @app.route('/get_topics', methods=['GET'])
-def get_topics():
+def send_topics():
     app.logger.debug("sent topics")
-    return jsonify({'topics':[
-        "food","tech","finance"
-    ]})
+    return jsonify({'topics': ["food", "tech", "finance"]})
 
 # join network
+# NEEDS TO CHANGE
 @app.route('/join_network', methods=['POST'])
 def new_p2p():
     # get ip address of new p2p node
@@ -78,5 +282,8 @@ def new_p2p():
     return jsonify({'p2p_id': p2p_id })
 
 if __name__ == "__main__":
+    scheduler.init_app(app)
+    scheduler.start()
+
     # run flask app
     app.run(host="0.0.0.0", port=5000)
--- main_cluster/p2p-node/app.py
+++ main_cluster/p2p-node/app.py
@@ -1,11 +1,24 @@
 from flask import Flask, request, jsonify, session, g
 from flask_apscheduler import APScheduler
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 from collections import defaultdict
+import requests, socket, logging, time, sys, threading,os
 
-import requests, socket, logging, time, sys
+# CONSTANTS
+RETRY_COUNT = 3
+
+'''
+GLOBAL VARIABLES
+when using global variables, use the global_var dictionary
+'''
+global_var = defaultdict(lambda: None)
 
 # creates app
 app = Flask(__name__)
+
+# scheduler
+scheduler = APScheduler()
 
 '''
 PRINT STATEMENTS DO NOT WORK
@@ -18,17 +31,267 @@ the following can be used to output debug messages to kubectl logs:
 # following line makes debug messages visible in kubectl logs
 app.logger.setLevel(logging.DEBUG)
 
-'''
-GLOBAL VARIABLES
-when using global variables, use the global_var dictionary
-'''
-global_var = defaultdict(lambda: None)
+# SQL database
+mongo_client = MongoClient('mongodb://localhost:27017')
+db = mongo_client.my_database
 
-# CONSTANTS
-RETRY_COUNT = 3
+users_db = db['users']
 
-# scheduler
-scheduler = APScheduler()
+leaders_db = db['leaders']
+subscribed_topics_db = db['subscribed_topics']
+topic_neighbors_db = db['topic_neighbors']
+posts_db = db['posts']
+
+# lock mechanism
+class ReadAndWriteLock():
+    def __init__(self):
+        self.write_lock = threading.Lock()
+        self.readers_lock = threading.Lock()
+        self.readers_count = 0
+    
+    def acquire_writelock(self):
+        self.write_lock.acquire()
+
+    def release_writelock(self):
+        self.write_lock.release()
+
+    def acquire_readlock(self):
+        self.readers_lock.acquire()
+        self.readers_count += 1
+        if self.readers_count == 1:
+            self.write_lock.acquire()
+        self.readers_lock.release()
+
+    def release_readlock(self):
+        self.readers_lock.acquire()
+        self.readers_count -= 1
+        if self.readers_count == 0:
+            self.write_lock.release()
+        self.readers_lock.release()
+    
+rw_locks = defaultdict(lambda:ReadAndWriteLock())
+
+class User:
+    def __init__(self,name,email):
+        self.name = name
+        self.email = email
+
+class Leader:
+    def __init__(self,ip_address):
+        self.ip_address = ip_address
+
+class SubscribedTopic:
+    def __init__(self,name):
+        self.name = name
+
+class TopicNeighbor:
+    def __init__(self,ip_address,topic):
+        self.ip_address = ip_address
+        self.topic = topic
+
+class Post:
+    def __init__(self, topic,ip_address,text,timestamp):
+        self.topic = topic
+        self.ip_address = ip_address
+        self.text = text
+        self.timestamp = timestamp
+
+# USER
+# create user
+# returns mongodb_id of user
+'''
+@app.route('/create_user', methods=['POST'])
+def http_create_user():
+    name = request.json.get('name')
+    email = request.json.get('email')
+    user = create_user(name, email)
+    app.logger.debug(user)
+'''
+
+# create user
+def create_user(name,email):
+    new_user = User(name, email)
+    user_id = users_db.insert_one(new_user.__dict__).inserted_id
+    user = users_db.find_one({'_id': ObjectId(user_id)})
+    user["_id"] = str(user["_id"])
+    return user_id
+
+# get users
+# returns list of users
+def get_users():
+    users = []
+    for user in users_db.find():
+        user['_id'] = str(user['_id'])
+        users.append(user)
+    return users
+
+# get user
+def get_user(user_id):
+    if user_id == None or user_id == "":
+        raise ValueError('user_id is missing')
+
+    user = users_db.find_one({'_id': ObjectId(user_id)})
+
+    if user:
+        user["_id"] = str(user["_id"])
+        return user
+    else:
+        raise ValueError('User not found')
+
+# update user
+def update_user(user_id,name,email):
+    if user_id == None or user_id == "":
+        raise ValueError('user_id is missing')
+    if name == None or name == "":
+        raise ValueError('name is missing')
+    if email == None or email == "":
+        raise ValueError('email is missing')
+    
+    result = users_db.update_one(
+        {'_id': ObjectId(user_id)}, 
+        {'$set': {'name': name, 'email': email}}
+    )
+    if result.modified_count != 1:
+        raise ValueError('User not found')
+
+# delete user
+def delete_user(user_id):
+    result = users_db.delete_one({'_id': ObjectId(user_id)})
+    if result.deleted_count != 1:
+        raise ValueError('User not found')
+
+# LEADER
+# set the leader
+def set_leader(ip_address):
+    rw_locks["leader"].acquire_writelock()
+    for leader in leaders_db.find():
+        leader__id = str(leader['_id'])
+        result = leaders_db.delete_one({'_id': ObjectId(leader__id)})
+        if result.deleted_count != 1:
+            raise ValueError('Leader not found')
+        
+    new_leader = Leader(ip_address=ip_address)
+    leaders_db.insert_one(new_leader.__dict__).inserted_id
+    rw_locks["leader"].release_writelock()
+
+# get the leader
+# return ip address of the leader
+def get_leader():
+    rw_locks["leader"].acquire_readlock()
+    leader = leaders_db.find_one()
+    rw_locks["leader"].release_readlock()
+
+    if leader is not None:
+        app.logger.debug("leader found")
+        return leader["ip_address"]
+    else:
+        app.logger.debug("leader not found")
+        return None
+
+# SubscribedTopic
+# create topic
+def create_subscribed_topic(name):
+    rw_locks["topic"].acquire_writelock()
+    topic = subscribed_topics_db.find_one({'name':name})
+    if topic is not None:
+        rw_locks["topic"].release_writelock()
+        app.logger.debug("topic already exists")
+        return False
+    new_topic = SubscribedTopic(name=name)
+    topic_id = subscribed_topics_db.insert_one(new_topic.__dict__).inserted_id
+    rw_locks["topic"].release_writelock()
+
+# get subscribed topics
+def get_subscribed_topics():
+    rw_locks["topic"].acquire_readlock()
+    topics = subscribed_topics_db.find()
+    rw_locks["topic"].release_readlock()   
+    return [topic["name"] for topic in topics]
+
+# get specific subscribed topic
+def get_subscribed_topic(name):
+    rw_locks["topic"].acquire_readlock()
+    topic = subscribed_topics_db.find_one({'name':name})
+    rw_locks["topic"].release_readlock()   
+    return topic
+
+# delete subscribed topic
+# returns whether deleted
+def delete_subscribed_topic(name):
+    rw_locks["topic"].acquire_writelock()
+    result = subscribed_topics_db.delete_one({'name': name})
+    if result.deleted_count != 1:
+        app.logger.debug("topic not found")
+    rw_locks["topic"].release_writelock()
+
+# TopicNeighbor
+# create topic neighbor
+def create_topic_neighbor(ip_address,topic):
+    rw_locks["topic-neighbor"].acquire_writelock()
+    new_neighbor = TopicNeighbor(ip_address=ip_address,topic=topic)
+    topic_id = topic_neighbors_db.insert_one(new_neighbor.__dict__).inserted_id
+    rw_locks["topic-neighbor"].release_writelock()
+    return str(topic_id)
+
+# get topic neighbors
+# return list of ip addresses
+def get_topic_neighbors(topic):
+    rw_locks["topic-neighbor"].acquire_readlock()
+    neighbors = topic_neighbors_db.find({'topic':topic})
+    rw_locks["topic-neighbor"].release_readlock()
+    return [neighbor["ip_address"] for neighbor in neighbors]
+
+# gets ip addresses of each topic neighbor
+# return list of ip addresses
+def get_topic_neighbors_from_all_topics():
+    rw_locks["topic-neighbor"].acquire_readlock()
+    neighbors = topic_neighbors_db.find()
+    rw_locks["topic-neighbor"].release_readlock()
+    return list(set([neighbor["ip_address"] for neighbor in neighbors]))
+
+# delete a neighbor from all topics
+def delete_neighbor_from_all_topics(ip_address):
+    rw_locks["topic-neighbor"].acquire_writelock()
+    result = topic_neighbors_db.delete_many({'ip_address':ip_address})
+    rw_locks["topic-neighbor"].release_writelock()
+
+# delete all neighbors of from a topics
+def delete_all_neighbors_from_a_topic(topic):
+    rw_locks["topic-neighbor"].acquire_writelock()
+    results = topic_neighbors_db.delete_many({'topic':topic})
+    rw_locks["topic-neighbor"].release_writelock()
+
+# Post
+# create post
+def create_post(topic,ip_address,text,timestamp):
+    rw_locks["post"].acquire_writelock()
+    new_post = Post(topic=topic,ip_address=ip_address,text=text,timestamp=timestamp)
+    post_id = posts_db.insert_one(new_post.__dict__).inserted_id
+    rw_locks["post"].release_writelock()
+
+# get posts by topic
+def get_posts_by_topic(topic):
+    rw_locks["post"].acquire_readlock()
+    posts = posts_db.find({'topic':topic})
+    rw_locks["post"].release_readlock()
+    return [post for post in posts]
+
+# example of a function scheduled periodically with scheduler
+def print_job():
+    app.logger.debug("print job")
+    app.logger.debug(global_var["ip_address"])
+    app.logger.debug(global_var["p2p_id"])
+
+    if not global_var["turn"]:
+        app.logger.debug("create tom hanks user")
+        create_user(name="Tom Hanks", email="tomhanks@gmail.com")
+
+        global_var["turn"] = True
+    else:
+        users = get_users()
+        app.logger.debug(users)
+
+        global_var["turn"] = False
 
 '''
 given a request call, attempt_request attempts to
@@ -41,7 +304,7 @@ def attempt_request(request_func):
     attempts = RETRY_COUNT
     while attempts > 0:
         # attempt to send request
-        try:            
+        try:
             response = request_func()
             response.raise_for_status() 
             app.logger.debug("request successful!")
@@ -58,11 +321,6 @@ def attempt_request(request_func):
                 time.sleep(5)    
     return None
 
-# example of a function scheduled periodically with scheduler
-def print_job():
-    app.logger.debug("print job")
-    app.logger.debug(global_var["ip_address"])
-    app.logger.debug(global_var["p2p_id"])
 
 # join network and update ["p2p_id"]
 def join_network():
@@ -100,7 +358,6 @@ def get_topics():
     app.logger.debug("p2p node retrieved topics")
 
 if __name__ == "__main__":
-
     # join network and get topics
     join_network()
     get_topics()
@@ -109,15 +366,96 @@ if __name__ == "__main__":
     if global_var["topics"] == None:
         sys.exit()
 
-    # initialize and start scheduler
-    # the following two lines are executed once
     scheduler.init_app(app)
     scheduler.start()
 
     # add job
     # jobs can be added and removed add any time
     # even in functions and route functions
-    scheduler.add_job(id='print_job', func=print_job, trigger='interval', seconds=10)
+    # scheduler.add_job(id='print_job', func=print_job, trigger='interval', seconds=10)
+
+
+    app.logger.debug("USERS")
+    user_id = create_user(name="Tom Hanks", email="tomhanks@gmail.com")
+    users = get_users()
+    app.logger.debug("-retrieved users")
+    app.logger.debug(users)
+
+    update_user(user_id,name="Keanu Reeves",email="keanureeves@gmail.com")
+    app.logger.debug("-updated user")
+    user = get_user(user_id)
+    app.logger.debug(user)
+
+    delete_user(user_id)
+    app.logger.debug("deleted user")
+    users = get_users()
+    app.logger.debug("retrieved users")
+    app.logger.debug(users)
+
+    app.logger.debug("LEADER")
+    app.logger.debug("-no leader")
+    leader = get_leader()
+    app.logger.debug(leader)
+
+    set_leader("1234")
+    app.logger.debug("-new leader")
+    leader = get_leader()
+    app.logger.debug(leader)
+
+    app.logger.debug("SUBSCRIBED TOPICS")
+    app.logger.debug("-no subscribed topics")
+    topics = get_subscribed_topics()
+    app.logger.debug(topics)
+
+    app.logger.debug("-adding food")
+    create_subscribed_topic("food")
+    topics = get_subscribed_topics()
+    app.logger.debug(topics)
+
+    app.logger.debug("-get food")
+    topic = get_subscribed_topic("food")
+    app.logger.debug(topic)
+
+    app.logger.debug("-deleted subscribed topics")
+    topic = delete_subscribed_topic("food")
+    topics = get_subscribed_topics()
+    app.logger.debug(topics)
+
+    app.logger.debug("TOPIC NEIGHBORS")
+
+    app.logger.debug("-add topic neighbor")
+    create_topic_neighbor("789","food")
+    create_topic_neighbor("123","food")
+    create_topic_neighbor("789","tech")
+    create_topic_neighbor("123","tech")
+    create_topic_neighbor("567","tech")
+
+    app.logger.debug("-get neighbors from a topic")
+    neighbors = get_topic_neighbors("food")
+    app.logger.debug(neighbors)
+    app.logger.debug("-get all neighbors")
+    neighbors = get_topic_neighbors_from_all_topics()
+    app.logger.debug(neighbors)
+
+    delete_neighbor_from_all_topics("123")
+    app.logger.debug("-123 was deleted from food")
+    neighbors = get_topic_neighbors("food")
+    app.logger.debug(neighbors)
+    app.logger.debug("-123 was deleted from tech")
+    neighbors = get_topic_neighbors("tech")
+    app.logger.debug(neighbors)
+
+    delete_all_neighbors_from_a_topic("tech")
+    app.logger.debug("- all neighbors in tech were deleted")
+    neighbors = get_topic_neighbors("tech")
+    app.logger.debug(neighbors)
+
+    app.logger.debug("POSTS")
+    app.logger.debug("create post")
+    post_id = create_post(topic="food",ip_address="1234",text="hello world",timestamp="1234")
+    app.logger.debug("get posts")
+    posts = get_posts_by_topic("food")
+    app.logger.debug(posts)
 
     # run flask app
     app.run(host="0.0.0.0", port=5000)
